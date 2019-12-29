ec2fe99c5e6c72a03ff19744533169aa56ad2fe9
Betsalel "Saul <9617959+betsalel-williamson@users.noreply.github.com>
2019-12-29 13:23:46 -0500

    Update main.c
    updated file header

--- T1/step-4-hello-world/3-Makefile-Hello-World/main.c
+++ T1/step-4-hello-world/3-Makefile-Hello-World/main.c
@@ -1,4 +1,13 @@
 /*
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+*/
+
+/*
  * Welcome to your second program in C. 
  * 
  * In this program you will modify the code from your first program 
