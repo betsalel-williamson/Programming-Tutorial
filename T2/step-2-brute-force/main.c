bc479f527be40053b38d9fca1ab67052913d2af3
Betsalel "Saul <9617959+betsalel-williamson@users.noreply.github.com>
2019-12-29 13:29:35 -0500

    Update main.c

--- T2/step-2-brute-force/main.c
+++ T2/step-2-brute-force/main.c
@@ -1,5 +1,14 @@
 /* Trivial way to crack the code */
 
+/*
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
