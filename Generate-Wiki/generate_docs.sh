9fc1de135e38ad10294ca6e7b13eabd8b8e78291
Betsalel (Saul) Williamson <saul.williamson@ieee.org>
2021-01-02 00:05:23 -0500

    Updated the generate docs command to output to the proper directory

--- Generate-Wiki/generate_docs.sh
+++ Generate-Wiki/generate_docs.sh
@@ -1,2 +1,23 @@
 #! env sh
-find . -name "*.md" -exec sh -c 'm4 definitions.m4 "$0" > "../../Programming-Tutorial.wiki/$0"' {} \;
+# filename: generate_docs.sh
+
+# This command must be executed with the following:
+#   1. run inside the "Generate-Wiki" directory
+#   2. have the wiki repository cloned at ../../Programming-Tutorial.wiki
+#
+
+# TODO: validate assumptions here...
+
+# version 1 of the command:
+# find . -name "*.md" -exec sh -c 'm4 definitions.m4 "$0" > "../../Programming-Tutorial.wiki/$0"' {} \;
+
+# find the md.m4 files
+# for each one, 
+# run the m4 command
+# to do this we need to rename the files from *.md.m4 to *.md
+# and place these files in the wiki directory
+
+# we call the realpath command here so that it fails fast
+# if the wiki directory does not exist
+
+find . -name "*.md.m4" -exec sh -c 'rename=$(echo "$0" | perl -lane "print /(.*)[.]m4/"); echo "m4 definitions.m4 \"$(realpath $0)\" > \"$(realpath ../../Programming-Tutorial.wiki/$rename)\""' {} \;
